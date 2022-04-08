18695b4f63166feeb46390b959bba6e472950de1
Stefanava <avanessian@hotmail.co.uk>
2022-04-08 12:06:04 +0100

    Edit wake up script to not cast response to variable

--- public/js/utils/wake-up-app.js
+++ public/js/utils/wake-up-app.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 module.exports = async () => {
-	let events = await fetch(`${process.env.APP_BASE_URL}/`, {
+	await fetch(`${process.env.APP_BASE_URL}/`, {
 		method: 'GET',
 		headers: {
 			'Accept': 'application/json',
